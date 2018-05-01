2459b6c526a44a855a4c13c0830efea82274bfdd
dessyd <dominique.dessy@gmail.com>
2018-05-01 18:29:54 +0200

    Added check commands exists

--- test-internet.sh
+++ test-internet.sh
@@ -8,6 +8,10 @@ HOST="www.dessy.be"
 PORT=80
 FACILITY="syslog.warn"
 TAG="network"
+
+hash curl 2>/dev/null || { echo >&2 "I require curl but it's not installed.  Aborting."; exit 1; }
+hash logger 2>/dev/null || { echo >&2 "I require logger but it's not installed.  Aborting."; exit 1; }
+
 if ! $(curl --output /dev/null --silent --head --fail http://$HOST:$PORT)
 then
   logger -i -p $FACILITY -t $TAG "Internet Unreachable"
