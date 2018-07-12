aca6951fa4789e64414e91fbf4a94898874fb562
dessyd <dominique.dessy@gmail.com>
2018-07-12 13:58:41 +0200

    Store flag in script directory

--- test-internet.sh
+++ test-internet.sh
@@ -8,18 +8,22 @@ HOST="www.dessy.be"
 PORT=80
 FACILITY="syslog.warn"
 TAG="network"
+#
+# Store Flag in the same dorectory as the script
+#
+FLAG=`dirname $0`/.down
 
 hash curl 2>/dev/null || { echo >&2 "I require curl but it's not installed.  Aborting."; exit 1; }
 hash logger 2>/dev/null || { echo >&2 "I require logger but it's not installed.  Aborting."; exit 1; }
 
 if ! $(curl --output /dev/null --silent --head --fail http://$HOST:$PORT)
 then
-  touch .down
+  touch $FLAG
   logger -i -p $FACILITY -t $TAG "Internet Unreachable"
 else
-  if [ -e .down ]
+  if [ -e $FLAG ]
   then
-    rm .down
+    rm $FLAG
     logger -i -p $FACILITY -t $TAG "Internet Restored"
   fi
 fi
